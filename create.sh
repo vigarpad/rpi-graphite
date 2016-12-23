c089437dac9de707c4b32c3f2063d694875ce0f6
Benedikt Heine <bebe@bebehei.de>
2016-12-23 13:26:56 +0100

    add creation-file to give hints how to use volumes

---
+++ create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CONTAINERNAME=graphite
+BASE=$PWD
+
+docker create \
+	--name $CONTAINERNAME \
+	--hostname $CONTAINERNAME \
+	--volume $BASE/lib:/var/lib/graphite \
+	--volume $BASE/log/apache:/var/log/apache2 \
+	--volume $BASE/log/graphite:/var/log/graphite \
+	--volume $BASE/log/carbon:/var/log/carbon \
+	bebehei/graphite:latest
+
+echo Created graphite container. You can run this now via docker start graphite.
